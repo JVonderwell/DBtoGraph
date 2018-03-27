6f17af71487c6b3142ab5da9247e84a59fb713ef
JVonderwell <jvonderwell@gmail.com>
2018-03-26 19:58:39 -0500

    added BCNF functionality

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import click
 import sys
+import pandas as pd
 
 
 class Parser:
@@ -28,14 +29,69 @@ class Parser:
             print str(edge[0]) + ' ' + str(edge[1])
 
 
+class BCNFSplitter:
+
+    def __init__(self, data_file, fd_file):
+        self.data = pd.read_csv(data_file)
+
+        # dict mapping list of LHS to string RHS
+        self.fds = list()
+
+        fdf = open(fd_file, 'r')
+
+        for line in fdf:
+            split_fd = line.split('-')
+            left = tuple(split_fd[0].split(','))
+            right = split_fd[1].rstrip('\n')
+            self.fds.append([left, right])
+
+    def split_data(self):
+        # returns normalized list of list of column indices
+        # ex: [[1, 3], [1,2,4]]
+        tables = [range(self.data.shape[1])]
+        i = 0
+        while i < len(tables):
+            table = self.data.iloc[:, tables[i]].drop_duplicates()
+            viol_fd = self._violated_fd(table)
+            if viol_fd != -1:
+                a = self.data.columns.get_loc(self.fds[viol_fd][1])
+                x = [self.data.columns.get_loc(col)
+                     for col in self.fds[viol_fd][0]]
+
+                # XA
+                x.append(a)
+                tables.append(x)
+
+                # R - A
+                table = tables.pop(i)
+                table.remove(a)
+                tables.append(table)
+
+                i -= 1
+            i += 1
+
+        return tables
+
+    def _violated_fd(self, table):
+        # returns index of violated fd or -1 if none
+        for fd in self.fds:
+            left = fd[0]
+            right = fd[1]
+            if all([s in table.columns for s in left]) \
+                    and right in table.columns \
+                    and not table.shape[1] == len(left) + 1:
+                    if any(table.groupby(left)[right].nunique() > 1):
+                        return self.fds.index(fd)
+
+        return -1
+
+
 @click.command()
 @click.option('--header', default=True, help='bool: Header for Data File')
 @click.option('--fdfile', default=None, help='Functional Dependency File Name')
 @click.option('--outfile', default=None, help='Output File Name')
 @click.argument('filename')
 def csv_to_graph(filename, header, fdfile, outfile):
-    if fdfile is not None:
-        print "Not supported yet"
 
     if outfile is not None:
         sys.stdout = open(outfile, 'w')
@@ -45,14 +101,24 @@ def csv_to_graph(filename, header, fdfile, outfile):
     f = open(filename, 'r')
 
     if header is True:
-        f.readline()
+        colnames = f.readline()
+
+    if fdfile is not None:
+        splitter = BCNFSplitter(filename, fdfile)
+        tables = splitter.split_data()
+    else:
+        tables = [range(len(colnames))]
 
     for line in f:
         split = line.split(',')
-        for i in range(len(split)-1):
-            parser.add_edge(split[i], i, split[i+1], i+1)
-        last_col = len(split) - 1
-        parser.add_edge(split[last_col], last_col, split[0], 0)
+        for table in tables:
+            for i in range(len(table) - 1):
+                col_1 = table[i]
+                col_2 = table[i+1]
+                parser.add_edge(split[col_1], col_1, split[col_2], col_2)
+            col_1 = table[0]
+            col_2 = table[len(table) - 1]
+            parser.add_edge(split[col_2], col_2, split[col_1], col_1)
 
     parser.print_edges()
 

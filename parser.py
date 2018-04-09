d26a6df15be6a04438460ed5b51639c319068d66
JVonderwell <jvonderwell@gmail.com>
2018-04-09 12:25:48 -0500

    added weighting by occurrence count

--- parser.py
+++ parser.py
@@ -1,37 +1,45 @@
 import click
-import sys
 import pandas as pd
-
-
-class Node:
-    # a value, attribute pair
-
-    def __init__(self, val, col_index):
-        self.val = val
-        self.index = col_index
 
 
 class Parser:
 
     def __init__(self):
         self.edges = set()
+
+        # maps unique nodes to ints - just for output formatting
         self.int_map = dict()
         self.curr = 0
+
+        # maps edges to occurrence count
+        self.count_map = dict()
 
     def _map_node(self, node):
         if node not in self.int_map:
             self.int_map[node] = self.curr
             self.curr += 1
 
+    def _map_edge(self, edge):
+        if edge not in self.edges:
+            self.count_map[edge] = 1
+        else:
+            self.count_map[edge] += 1
+
     def add_edge(self, node_1, node_2):
         self._map_node(node_1)
         self._map_node(node_2)
 
-        self.edges.add((self.int_map[node_1], self.int_map[node_2]))
+        edge = (self.int_map[node_1], self.int_map[node_2])
+        self._map_edge(edge)
 
-    def print_edges(self, out=None):
+        self.edges.add(edge)
+
+    def print_edges(self, out, weighted):
         for edge in sorted(self.edges):
-            str_edge =str(edge[0]) + ' ' + str(edge[1])
+            str_edge = str(edge[0]) + ' ' + str(edge[1])
+            if weighted:
+                str_edge += ' ' + str(self.count_map[edge])
+
             if out is None:
                 print str_edge
             else:
@@ -98,16 +106,17 @@ class BCNFSplitter:
 
 
 @click.command()
-@click.option('--fdfile', default=None, help='Functional Dependency File Name')
-@click.option('--outfile', default=None, help='Output File Name')
+@click.option('--fdfile', '-f', default=None, help='Functional Dependency File Name')
+@click.option('--outfile', '-o', default=None, help='Output File Name')
+@click.option('--weighted', '-w', is_flag=True)
 @click.argument('filename')
-def csv_to_graph(filename, fdfile, outfile):
+def csv_to_graph(filename, fdfile, outfile, weighted):
 
     parser = Parser()
 
     f = open(filename, 'r')
 
-    colnames = f.readline()
+    colnames = f.readline().split(',')
 
     if fdfile is not None:
         splitter = BCNFSplitter(filename, fdfile)
@@ -121,15 +130,15 @@ def csv_to_graph(filename, fdfile, outfile):
             for i in range(len(table) - 1):
                 col_1 = table[i]
                 col_2 = table[i+1]
-                node_1 = Node(split[col_1], col_1)
-                node_2 = Node(split[col_2], col_2)
+                node_1 = (split[col_1], col_1)
+                node_2 = (split[col_2], col_2)
                 parser.add_edge(node_1, node_2)
 
             # add edges for last col to first col
             col_1 = table[0]
             col_2 = table[len(table) - 1]
-            node_1 = Node(split[col_1], col_1)
-            node_2 = Node(split[col_2], col_2)
+            node_1 = (split[col_1], col_1)
+            node_2 = (split[col_2], col_2)
             parser.add_edge(node_1, node_2)
 
     if outfile is not None:
@@ -137,7 +146,7 @@ def csv_to_graph(filename, fdfile, outfile):
     else:
         out = None
         
-    parser.print_edges(out)
+    parser.print_edges(out, weighted)
 
 
 if __name__ == '__main__':
